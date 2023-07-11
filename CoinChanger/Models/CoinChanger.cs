265e7ba69a4b43e032a9d059d36ea7227be82669
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-11 15:32:51 -0700

    update program.cs file

---
+++ CoinChanger/Models/CoinChanger.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+namespace CoinChanger.Models
+{
+  public class CoinChanger
+  {
+
+  }
+}
---
+++ CoinChanger/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using CoinChanger.Models;
+using System.Collections.Generic;
+
+namespace CoinChanger
+{
+  class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
